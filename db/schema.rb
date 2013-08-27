29502a9f5641d02bb0e8c05777f9051cc5eb4ee2
Yuta Kato <yyttkktt@gmail.com>
2013-08-27 11:10:27 +0900

    migrationを実行
    $ rake db:migrate
    ==  CreateBlogs: migrating ====================================================
    -- create_table(:blogs)
       -> 0.0302s
    ==  CreateBlogs: migrated (0.0303s) ===========================================

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822162845) do
+ActiveRecord::Schema.define(version: 20130827020726) do
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "entries", force: true do |t|
     t.string   "title"
